1dc03e071d6874b0ec013a87e012c865e9befc02
Amir <amirmasoodjafari1677@gmail.com>
2025-11-15 15:17:29 +0100

    added feature request

--- pages/popup/src/components/Footer.tsx
+++ pages/popup/src/components/Footer.tsx
@@ -1,8 +1,14 @@
 export const Footer = () => (
   <div className="mt-auto flex items-center justify-between gap-2">
-    <div className="flex">
-      <a href="https://linkedin.com/in/amir-jafari/" target="_blank" className="text-xs text-gray-300">
+    <div className="flex flex-col gap-1">
+      <a href="https://linkedin.com/in/amir-jafari/" target="_blank" className="text-foreground text-xs">
         Built with ❤️ by <span className="font-bold">Amir</span>
+      </a>
+      <a
+        href="https://rewrite.canny.io/feature-requests/p/custom-instructions"
+        target="_blank"
+        className="text-foreground text-xs hover:underline">
+        Request a feature
       </a>
     </div>
 
@@ -10,7 +16,7 @@ export const Footer = () => (
       <img
         src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=☕&slug=amirjf&button_colour=FFDD00&font_colour=000000&font_family=Bree&outline_colour=000000&coffee_colour=ffffff"
         alt="Buy Me A Coffee"
-        className="h-16 w-32 object-contain"
+        className="h-16 w-36 object-contain"
       />
     </a>
   </div>
